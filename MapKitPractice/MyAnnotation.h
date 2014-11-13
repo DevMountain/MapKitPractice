417226240efa94d5a2a13f3179bed4561edc2396
Chase Wasden <chase@jane.com>
2014-11-12 21:51:14 -0700

    initial data

---
+++ MapKitPractice/MyAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.h
+//  MapKitPractice
+//
+//  Created by Chase Wasden on 11/11/14.
+//  Copyright (c) 2014 Chase Wasden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyAnnotation : NSObject
+
+@end
---
+++ MapKitPractice/MyAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.m
+//  MapKitPractice
+//
+//  Created by Chase Wasden on 11/11/14.
+//  Copyright (c) 2014 Chase Wasden. All rights reserved.
+//
+
+#import "MyAnnotation.h"
+
+@implementation MyAnnotation
+
+@end
